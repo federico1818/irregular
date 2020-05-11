9c6df3fe3b15beb51fd86d39c82e871d47bccd96
Federico González <federico1818@gmail.com>
2020-05-11 18:45:06 -0300

    add drawer container to contrast

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatTableModule } from '@angular/material/table'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSidenavModule } from '@angular/material/sidenav'
 
 
 
@@ -17,14 +18,16 @@ import { MatFormFieldModule } from '@angular/material/form-field'
         MatTableModule,
         MatIconModule,
         MatInputModule,
-        MatFormFieldModule
+        MatFormFieldModule,
+        MatSidenavModule
     ],
     exports: [
         MatToolbarModule,
         MatTableModule,
         MatIconModule,
         MatInputModule,
-        MatFormFieldModule
+        MatFormFieldModule,
+        MatSidenavModule
     ]
 })
 
